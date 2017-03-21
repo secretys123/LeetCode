dd6d6e736efe4b2a142691580cd6e4170427d9c4
secretys123 <secretys123@gmail.com>
2017-03-21 21:50:35 +0800

    Create Palindrome Number.java

---
+++ 9. Palindrome Number/Palindrome Number.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public boolean isPalindrome(int x) {
+        int num=x;
+        if(num<0){
+            return false;
+        }
+        String s=""+x;
+        int str=0;
+        int end=s.length()-1;
+        while(str<end){
+            char str_s=s.charAt(0);
+            char end_s=s.charAt(end);
+            if(str_s!=end_s){
+                return false;
+            }
+            str++;
+            end--;
+        }
+        return true;
+    }
+}
